2006a2780785a2ee051a390a24950699f9f29632
rolf <16788177+getsmokedhahaXDDDDD@users.noreply.github.com>
2018-12-18 19:06:59 -0500

    Update Module.js

--- Module.js
+++ Module.js
@@ -1,6 +1,6 @@
-load("import/Fly.js");
-load("import/required.js");
-load("import/Speed.js");
+load("https://raw.githubusercontent.com/getsmokedhahaXDDDDD/Proton-Modules-Repo/master/import/Fly.js");
+load("https://raw.githubusercontent.com/getsmokedhahaXDDDDD/Proton-Modules-Repo/master/import/required.js");
+load("https://raw.githubusercontent.com/getsmokedhahaXDDDDD/Proton-Modules-Repo/master/import/Speed.js");
 function addMods(modules) {
 	modules.add(new SpeedAddon());
 	modules.add(new FlyAddon());
