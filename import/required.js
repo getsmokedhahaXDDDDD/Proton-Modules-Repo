db7a8a4de37aed06314983526778e4582038c2d0
rolf <16788177+getsmokedhahaXDDDDD@users.noreply.github.com>
2018-12-18 20:19:24 -0500

    Update required.js

--- import/required.js
+++ import/required.js
@@ -1,6 +1,13 @@
 var Module =  Java.type("CPacket.Despacito.module.Module");
+var Category =  Java.type("CPacket.Despacito.module.Module.Category");
 var Minecraft =  Java.type("net.minecraft.client.Minecraft");
-var Client =  Java.type("CPacket.Despacito.Proton");
+var Client =  Java.type("CPacket.Despacito.Proton").proton;
 var MotionUtils =  Java.type("CPacket.Despacito.util.SpeedUtils");
+var MotionUtils =  Java.type("CPacket.Despacito.util.SpeedUtils");
+var Setting = Java.type("ooo.cpacket.lemongui.settings.Setting");
+var settings = Client.setmgr;
 var mc = Minecraft.getMinecraft();
 var playerInstance = Minecraft.getMinecraft().thePlayer;
+function addSetting(setting) {
+  settings.rSetting(setting);
+}
