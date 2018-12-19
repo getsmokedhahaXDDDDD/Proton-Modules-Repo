ba367e2121f63b53eefc5ded13d0274cd573ea8b
rolf <16788177+getsmokedhahaXDDDDD@users.noreply.github.com>
2018-12-18 19:04:41 -0500

    Create required.js

---
+++ import/required.js
@@ -0,0 +1,6 @@
+var Module =  Java.type("CPacket.Despacito.module.Module");
+var mc =  Java.type("net.minecraft.client.Minecraft");
+var Client =  Java.type("CPacket.Despacito.Proton");
+var MotionUtils =  Java.type("CPacket.Despacito.util.SpeedUtils");
+var mc = Minecraft.getMinecraft();
+var playerInstance = Minecraft.getMinecraft().thePlayer;
