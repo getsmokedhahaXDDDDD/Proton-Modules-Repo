925f8aeb25d68ceb9018a4fce565aec7a8f12b27
rolf <16788177+getsmokedhahaXDDDDD@users.noreply.github.com>
2018-12-20 21:44:16 -0500

    Update Module.js

--- Movement/Module.js
+++ Movement/Module.js
@@ -4,9 +4,14 @@ load("https://raw.githubusercontent.com/getsmokedhahaXDDDDD/Proton-Modules-Repo/
 var speed = new SpeedAddon();
 var fly = new FlyAddon();
 function addMods(modules) {
-	modules.add(fly);
-	modules.add(speed);
+  modules.add(fly);
+  modules.add(speed);
 }
 function addSettings(setmgr) {
-	
+  var list = new ArrayList();
+  list.add('Vanilla');
+  list.add('Velt');
+  list.add('Hypixel');
+  fly.addModes(list);
+  setmgr.rSetting(new Setting("Speed", fly, 0.493, 0.125, 5.0, false));
 }
