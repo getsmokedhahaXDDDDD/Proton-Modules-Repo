dddf173ff45aff97b98af80d23dc39d3a7eadab3
rolf <16788177+getsmokedhahaXDDDDD@users.noreply.github.com>
2018-12-21 15:27:42 -0500

    Update Speed.js

--- import/Speed.js
+++ import/Speed.js
@@ -1,1 +1,26 @@
 loadWithNewGlobal("https://raw.githubusercontent.com/getsmokedhahaXDDDDD/Proton-Modules-Repo/master/import/required.js");
+var FlyAddon = Java.extend(Module, {
+	getName: function() {
+		return "Speed";
+	},
+  onEnable: function() {
+		
+	},
+	onDisable: function() {
+		
+	},
+	getCategory: function() {
+		return Category.MOVE;
+	},
+	JSInit: function() {
+	},
+	move: function(moveEvent) {
+		
+	},
+	update: function(motionEvent) {
+		if (playerInstance.onGround && playerInstance.isMoving()) {
+        playerInstance.motionY = 0.42;
+		}
+		MotionUtils.setMoveSpeed(0.4255);
+	}
+});
