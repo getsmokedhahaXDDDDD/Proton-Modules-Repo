e266413867b24557425530d02b622d26ab242b1c
rolf <16788177+getsmokedhahaXDDDDD@users.noreply.github.com>
2018-12-20 20:01:13 -0500

    Update Fly.js

--- import/Fly.js
+++ import/Fly.js
@@ -1,5 +1,4 @@
 load("https://raw.githubusercontent.com/getsmokedhahaXDDDDD/Proton-Modules-Repo/master/import/required.js");
-load("https://raw.githubusercontent.com/getsmokedhahaXDDDDD/Proton-Modules-Repo/master/Movement/Module.js");
 var FlyAddon = Java.extend(Module, {
 	getName: function() {
 		return "Fly";
