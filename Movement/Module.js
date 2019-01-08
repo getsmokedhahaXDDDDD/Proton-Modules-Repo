09ad33bef51cdcce34626ba1d6136fd6ec420501
rolf <16788177+getsmokedhahaXDDDDD@users.noreply.github.com>
2019-01-08 16:12:43 -0500

    Update Module.js

--- Movement/Module.js
+++ Movement/Module.js
@@ -1,9 +1,9 @@
-loadWithNewGlobal("https://raw.githubusercontent.com/getsmokedhahaXDDDDD/Proton-Modules-Repo/master/import/Fly.js");
+//TODO: loadWithNewGlobal("https://raw.githubusercontent.com/getsmokedhahaXDDDDD/Proton-Modules-Repo/master/import/Fly.js");
 loadWithNewGlobal("https://raw.githubusercontent.com/getsmokedhahaXDDDDD/Proton-Modules-Repo/master/import/Speed.js");
 var speed = new SpeedAddon();
-var fly = new FlyAddon();
+//var fly = new FlyAddon();
 function addMods(modules) {
-  modules.add(fly);
+  //modules.add(fly);
   modules.add(speed);
 }
 function addSettings(setmgr) {
