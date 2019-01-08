84e5ef4bd896f1ddf35e8c6b78fc416ae51a7901
rolf <16788177+getsmokedhahaXDDDDD@users.noreply.github.com>
2019-01-08 16:45:29 -0500

    Update Speed.js

--- import/Speed.js
+++ import/Speed.js
@@ -1,5 +1,5 @@
 load("https://raw.githubusercontent.com/getsmokedhahaXDDDDD/Proton-Modules-Repo/master/import/required.js");
-var FlyAddon = Java.extend(Module, {
+var SpeedAddon = Java.extend(Module, {
 	getName: function() {
 		return "VeltRapeXD";
 	},
